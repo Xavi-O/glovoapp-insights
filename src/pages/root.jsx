85e989663f0da774ebe9682914f18e4813a12cb1
Xavier <odhiamboxavier@gmail.com>
2024-01-15 12:47:57 +0300

    Responsive navbar

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -15,50 +15,55 @@ export default function Root() {
     <>
       <div className="flex">
         <nav
-          className=" m-1 w-1/6 h-full sticky top-0 bg-zinc-50"
+          className=" m-1 w-1/12 md:w-1/6 h-screen sticky top-0 bg-zinc-50"
           id="sidebar"
         >
           <Link className="" to={`/home`}>
             <img
-              className="w-1/2 mx-auto mt-2"
+              className="w-11/12 md:w-1/2 mx-auto mt-2 hidden sm:block"
               alt="glovo logo"
               src="glovo-logo.svg"
             ></img>
+            <img
+              className="w-4/5 sm:w-1/3 md:mx-auto mt-2 sm:hidden"
+              alt="glovo logo"
+              src="glovoapp-icon.svg"
+            ></img>
           </Link>
           
-          <TabGroup className="ml-4">
-            <TabList color="emerald" className="w-3/5">
+          <TabGroup className="ml-0 md:ml-4">
+            <TabList color="emerald" className="w-5/6 md:w-5/6 lg:w-3/5">
               <NavLink to={`/home`}>
                 <Tab className="mt-8" icon={HomeIcon}>
-                  Home
+                  <p className="hidden md:block">Home</p>
                 </Tab>
               </NavLink>
             </TabList>
-            <TabList color="emerald" className="w-3/5">
+            <TabList color="emerald" className="w-5/6 md:w-5/6 lg:w-3/5">
               <NavLink to={`/dashboard`}>
                 <Tab className="mt-8" icon={ChartPieIcon}>
-                  Dashboard
+                  <p className="hidden md:block">Dashboard</p>
                 </Tab>
               </NavLink>
             </TabList>
-            <TabList color="emerald" className="w-3/5">
+            <TabList color="emerald" className="w-5/6 md:w-5/6 lg:w-3/5">
               <NavLink to={`/stores`}>
                 <Tab className="mt-8" icon={OfficeBuildingIcon}>
-                  Stores
+                <p className="hidden md:block">Stores</p>
                 </Tab>
               </NavLink>
             </TabList>
-            <TabList color="emerald" className="w-3/5">
+            <TabList color="emerald" className="w-5/6 md:w-5/6 lg:w-3/5">
               <NavLink to={`/kfc-store`}>
                 <Tab className="mt-8" icon={ShoppingBagIcon}>
-                  KFC
+                <p className="hidden md:block">KFC</p>
                 </Tab>
               </NavLink>
             </TabList>
-            <TabList color="red" className="w-3/5">
+            <TabList color="red" className="w-5/6 md:w-5/6 lg:w-3/5">
               <NavLink to={`/logout`}>
                 <Tab className="mt-8" icon={StopIcon}>
-                  Logout
+                <p className="hidden md:block">Logout</p>
                 </Tab>
               </NavLink>
             </TabList>
