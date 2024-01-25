658163d7895c386e1bd1e79fcb195c517cf30a03
Xavier <odhiamboxavier@gmail.com>
2024-01-25 05:59:33 +0300

    default date

--- src/pages/kfc.jsx
+++ src/pages/kfc.jsx
@@ -118,7 +118,7 @@ export default function Kfc() {
           <DatePicker
             id="datePicker"
             className="w-1/4 mr-6"
-            defaultValue={date}
+            defaultValue={new Date('1/24/2024')}
             enableClear={false}
             displayFormat="M/dd/yyyy"
             minDate={new Date('1/24/2024')}
